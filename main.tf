bceabb5b0aa916602269473410a58d5e94505d76
suresh46 <sureshjava600@gmail.com>
2023-06-15 13:04:26 +0530

    12 commit

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "everything fine" > /var/www/html/index.html
+              echo "pipeline working fine" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
